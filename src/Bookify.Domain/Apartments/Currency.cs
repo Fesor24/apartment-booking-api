8f9e1e39a4c0d21cb3221d22b5d6e691153f21cc
Wale <onafesowale@gmail.com>
2024-10-31 18:31:14 +0100

    bookify-domain

---
+++ src/Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,21 @@
+﻿namespace Bookify.Domain.Apartments;
+public record Currency
+{
+    internal static readonly Currency None = new("");
+    public static readonly Currency Usd = new ("USD");
+    public static readonly Currency Eur = new("EUR");
+    public Currency(string code)
+    {
+        Code = code;
+    }
+    public string Code { get; init; }
+    public static Currency FromCode(string code) =>
+        All.FirstOrDefault(x => x.Code == code) ?? 
+        throw new ApplicationException();
+
+    public static readonly IReadOnlyCollection<Currency> All = new[]
+    {
+        Usd,
+        Eur
+    };
+}
---
+++ src/Bookify.Domain/Apartments/Money.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookify.Domain.Apartments;
+public record Money(decimal Amount, Currency Currency)
+{
+    public static Money operator +(Money a, Money b)
+    {
+        if (a.Currency != b.Currency) throw new InvalidOperationException("Money currency are not the same");
+
+        return new Money(a.Amount + b.Amount, a.Currency);
+    }
+
+    public static Money Zero() => new(0, Currency.None);
+}
