c405f4f5bc6193c3fed5608257af475656eb6a23
Wale <onafesowale@gmail.com>
2024-12-20 08:23:11 +0100

    optimistic concurrency and publish domain events

--- src/Bookify.Infrastructure/Configuration/ApartmentConfiguration.cs
+++ src/Bookify.Infrastructure/Configuration/ApartmentConfiguration.cs
@@ -34,6 +34,8 @@ namespace Bookify.Infrastructure.Configuration
                 priceBuilder.Property(money => money.Currency)
                     .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
             });
+
+            builder.Property<uint>("Version").IsRowVersion(); // define a shadow property for optimistic concurrency...
         }
     }
 }
---
+++ src/Bookify.Infrastructure/Data/DateOnlyTypeHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using Dapper;
+
+namespace Bookify.Infrastructure.Data
+{
+    // Bcos dapper does not support DateOnly out of the box...hence we create a type handler
+    // Register handler in DI
+    internal sealed class DateOnlyTypeHandler : SqlMapper.TypeHandler<DateOnly>
+    {
+        public override DateOnly Parse(object value) => DateOnly.FromDateTime((DateTime)value);
+
+        public override void SetValue(IDbDataParameter parameter, DateOnly value)
+        {
+            parameter.DbType = DbType.Date;
+            parameter.Value = value;
+        }
+    }
+}
